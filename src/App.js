9925d45309882efefd527abd9908f5aa570c71db
Ananth Narayan <narayanananth26@gmail.com>
2023-06-11 09:46:45 +0530

    improve code and design

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ export default function App() {
 	const [isOpen, setIsOpen] = useState(true);
 
 	function handlePrevious() {
-		if (step > 1) setStep(step - 1);
+		if (step > 1) setStep((currStep) => currStep - 1);
 	}
 
 	function handleNext() {
-		if (step < 3) setStep(step + 1);
+		if (step < 3) setStep((currStep) => currStep + 1);
 	}
 
 	function handleClose() {
-		setIsOpen(!isOpen);
+		setIsOpen((currState) => !currState);
 	}
 
 	return (
@@ -30,9 +30,9 @@ export default function App() {
 			{isOpen && (
 				<div className="steps">
 					<div className="numbers">
-						<div className={step >= 1 ? "active" : ""}>1</div>
-						<div className={step >= 2 ? "active" : ""}>2</div>
-						<div className={step >= 3 ? "active" : ""}>3</div>
+						<div className={step === 1 ? "active" : ""}>1</div>
+						<div className={step === 2 ? "active" : ""}>2</div>
+						<div className={step === 3 ? "active" : ""}>3</div>
 					</div>
 					<p className="message">
 						Step {step}: {messages[step - 1]}
@@ -40,13 +40,27 @@ export default function App() {
 
 					<div className="buttons">
 						<button
-							style={{ backgroundColor: "#7950f2", color: "fff" }}
+							style={
+								step > 1
+									? {
+											backgroundColor: "#7950f2",
+											color: "fff",
+									  }
+									: { cursor: "default" }
+							}
 							onClick={handlePrevious}
 						>
 							Previous
 						</button>
 						<button
-							style={{ backgroundColor: "#7950f2", color: "fff" }}
+							style={
+								step < 3
+									? {
+											backgroundColor: "#7950f2",
+											color: "fff",
+									  }
+									: { cursor: "default" }
+							}
 							onClick={handleNext}
 						>
 							Next
